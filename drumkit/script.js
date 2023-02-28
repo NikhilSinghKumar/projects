3c39f3c627c454a7525fc60d610dfa73ac3259b6
Nikhil Kumar <nikhilsk369@gmail.com>
2023-02-28 07:28:03 +0530

    Drum Kit

---
+++ drumkit/script.js
@@ -0,0 +1,23 @@
+window.addEventListener("keydown",(e)=>{
+    var audio= document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    // console.log(audio);
+    var key=document.querySelector(`.key[data-key="${e.keyCode}"]`)
+    if(audio){
+        audio.play();
+        audio.currentTime=0;
+        key.classList.add("playing");
+    }
+    return;
+})
+
+var kk=document.querySelectorAll(".key")
+kk.forEach((e)=>{
+    e.addEventListener('transitionend', remove)
+})
+
+function remove(k){
+    if(k.propertyName="transition"){
+        this.classList.remove("playing")
+    }
+    return;
+}
